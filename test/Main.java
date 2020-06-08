a6e26ac15e414714b3d2d4d1de1ebe0d8e2a5682
alexbodart <alexbodart59@hotmail.fr>
2020-06-08 18:46:03 +0100

    Création d'un main

--- test/Main.java
+++ test/Main.java
@@ -3,7 +3,7 @@ package test;
 import vue.Connexion;
 
 
-public class test_inscription {
+public class Main {
     public static void main(String[] args) {
         //UtilisateurDao utilisateurManager = new JpaUtilisateurDao();
         //Utilisateur utilisateur = utilisateurManager.find(Utilisateur.class, 1);
